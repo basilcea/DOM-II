8d9a0cc61c2dfbad676c5533357b3374f33a0583
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-14 12:37:29 +0100

    use of mouseover for navlinks in the funbus page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+const navLinks =  document.querySelectorAll('.nav-link')
+
+navLinks.forEach(link => link.onmouseover = ()=>{
+    link.style.color ='white';
+    link.style.backgroundColor='teal';
+    link.style.width= '25%';
+    link.style.height ='3rem';
+    link.style.padding = '0.5rem';
+    link.style.textAlign = 'center';
+    link.style.margin = '2px';
+    link.style.boxShadow ='0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19)'
+})
